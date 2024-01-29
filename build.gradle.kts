a03d9c7ef1789800ccb759f3df6e1fe573d72858
turtton <top.gear7509@turtton.net>
2024-01-29 23:12:47 +0900

    style: Format build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
                 "com.pinterest.ktlint:ktlint-cli-reporter-html:1.0.0",
                 "com.pinterest.ktlint:ktlint-cli-reporter-plain:1.0.0",
                 "com.pinterest.ktlint:ktlint-cli-reporter-sarif:1.0.0",
-                "com.pinterest.ktlint:ktlint-ruleset-standard:1.0.0"
+                "com.pinterest.ktlint:ktlint-ruleset-standard:1.0.0",
             )
         }
     }
